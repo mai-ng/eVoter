9eb9f9e4ebaae3de1583d765053886ea5b7a0a1f
btdiem <diembt@somwhere>
2014-01-19 18:21:45 +0100

    update evoterServer: modify create_tables.sql

--- eVoterServer/db/create_tables.sql
+++ eVoterServer/db/create_tables.sql
@@ -23,9 +23,9 @@ CREATE TABLE IF NOT EXISTS `answer` (
   PRIMARY KEY (`id`),
   KEY `answer_FKIndex1` (`question_id`),
   CONSTRAINT `FK_answer_question` FOREIGN KEY (`question_id`) REFERENCES `question` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=utf8;
 
--- Dumping data for table evoter.answer: ~8 rows (approximately)
+-- Dumping data for table evoter.answer: ~10 rows (approximately)
 DELETE FROM `answer`;
 /*!40000 ALTER TABLE `answer` DISABLE KEYS */;
 INSERT INTO `answer` (`id`, `question_id`, `answer_text`) VALUES
@@ -36,7 +36,9 @@ INSERT INTO `answer` (`id`, `question_id`, `answer_text`) VALUES
 	(5, 4, 'My brother'),
 	(6, 4, 'He broke his leg.'),
 	(7, 4, 'Two hours.'),
-	(8, 4, 'It\'s 2 o\'clock.');
+	(8, 4, 'It\'s 2 o\'clock.'),
+	(9, 2, 'add a new answer'),
+	(10, 2, 'add a new answer');
 /*!40000 ALTER TABLE `answer` ENABLE KEYS */;
 
 
@@ -55,18 +57,18 @@ CREATE TABLE IF NOT EXISTS `question` (
   CONSTRAINT `FK_question_user` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8;
 
--- Dumping data for table evoter.question: ~8 rows (approximately)
+-- Dumping data for table evoter.question: ~7 rows (approximately)
 DELETE FROM `question`;
 /*!40000 ALTER TABLE `question` DISABLE KEYS */;
 INSERT INTO `question` (`id`, `question_text`, `creation_date`, `question_type_id`, `user_id`, `parent_id`) VALUES
-	(1, 'Interface can implement an interface?', '2014-01-09 10:31:17', 2, 1, 0),
-	(2, 'Abstract can implement an interface?', '2014-01-09 10:31:17', 2, 1, 0),
-	(3, 'An class can implement how many interface?', '2014-01-09 10:31:17', 1, 1, 0),
-	(4, 'Match the questions with the answers', '2014-01-09 22:39:24', 5, 1, 0),
-	(5, 'Who are you waiting for?', '2014-01-09 22:40:19', 5, 1, 4),
-	(6, 'What happened to Bob?', '2014-01-09 22:43:35', 5, 1, 4),
-	(7, 'How long does it take you to get there?', '2014-01-09 22:44:40', 5, 1, 4),
-	(8, 'Can you tell me the time?', '2014-01-09 22:45:45', 5, 1, 4);
+	(1, 'Interface can implement an interface?', '2014-01-09 10:31:17', 1, 1, 0),
+	(2, 'Abstract can implement an interface?', '2014-01-09 10:31:17', 1, 1, 0),
+	(3, 'An class can implement how many interface?', '2014-01-09 10:31:17', 2, 1, 0),
+	(4, 'Match the questions with the answers', '2014-01-09 22:39:24', 6, 1, 0),
+	(5, 'Who are you waiting for?', '2014-01-09 22:40:19', 6, 1, 4),
+	(6, 'What happened to Bob?', '2014-01-09 22:43:35', 6, 1, 4),
+	(7, 'How long does it take you to get there?', '2014-01-09 22:44:40', 6, 1, 4),
+	(8, 'Can you tell me the time?', '2014-01-09 22:45:45', 6, 1, 4);
 /*!40000 ALTER TABLE `question` ENABLE KEYS */;
 
 
@@ -75,17 +77,18 @@ CREATE TABLE IF NOT EXISTS `question_type` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `question_type_value` char(100) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8;
 
--- Dumping data for table evoter.question_type: ~5 rows (approximately)
+-- Dumping data for table evoter.question_type: ~6 rows (approximately)
 DELETE FROM `question_type`;
 /*!40000 ALTER TABLE `question_type` DISABLE KEYS */;
 INSERT INTO `question_type` (`id`, `question_type_value`) VALUES
-	(1, 'Multiple Choice'),
-	(2, 'Yes/No'),
-	(3, 'Agree/Disagree'),
+	(1, 'Yes/No\r\n'),
+	(2, 'Multi Radio Button'),
+	(3, 'Multi Checkbox'),
 	(4, 'Slider'),
-	(5, 'Match');
+	(5, 'Input Answer'),
+	(6, 'Match');
 /*!40000 ALTER TABLE `question_type` ENABLE KEYS */;
 
 
@@ -96,24 +99,27 @@ CREATE TABLE IF NOT EXISTS `session` (
   `name` char(100) NOT NULL,
   `creation_date` datetime NOT NULL,
   `is_active` tinyint(1) DEFAULT NULL,
+  `user_id` int(10) unsigned NOT NULL DEFAULT '1',
   PRIMARY KEY (`id`),
   KEY `session_FKIndex1` (`subject_id`),
+  KEY `FK_session_user` (`user_id`),
+  CONSTRAINT `FK_session_user` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`),
   CONSTRAINT `FK_session_subject` FOREIGN KEY (`subject_id`) REFERENCES `subject` (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8;
 
 -- Dumping data for table evoter.session: ~9 rows (approximately)
 DELETE FROM `session`;
 /*!40000 ALTER TABLE `session` DISABLE KEYS */;
-INSERT INTO `session` (`id`, `subject_id`, `name`, `creation_date`, `is_active`) VALUES
-	(1, 1, 'subject_1_session_1', '2013-12-28 00:00:00', 1),
-	(2, 1, 'subject_1_session_2', '2013-12-28 12:50:24', 0),
-	(3, 1, 'subject_1_session_3', '2013-12-28 12:50:24', 0),
-	(4, 2, 'subject_2_session_4\r\n', '2013-12-28 12:50:24', 0),
-	(5, 2, 'subject_2_session_5', '2013-12-28 12:50:24', 0),
-	(6, 2, 'subject_2_session_6', '2013-12-28 12:50:24', 0),
-	(7, 3, 'subject_3_session_7', '2013-12-28 12:50:24', 0),
-	(8, 3, 'subject_3_session_8', '2013-12-28 12:50:24', 0),
-	(9, 3, 'subject_3_session_9', '2013-12-28 12:50:24', 0);
+INSERT INTO `session` (`id`, `subject_id`, `name`, `creation_date`, `is_active`, `user_id`) VALUES
+	(1, 1, 'subject_1_session_1', '2013-12-28 00:00:00', 1, 1),
+	(2, 1, 'subject_1_session_2', '2013-12-28 12:50:24', 0, 1),
+	(3, 1, 'subject_1_session_3', '2013-12-28 12:50:24', 0, 1),
+	(4, 2, 'subject_2_session_4\r\n', '2013-12-28 12:50:24', 0, 2),
+	(5, 2, 'subject_2_session_5', '2013-12-28 12:50:24', 0, 2),
+	(6, 2, 'subject_2_session_6', '2013-12-28 12:50:24', 0, 2),
+	(7, 3, 'subject_3_session_7', '2013-12-28 12:50:24', 0, 1),
+	(8, 3, 'subject_3_session_8', '2013-12-28 12:50:24', 0, 1),
+	(9, 3, 'subject_3_session_9', '2013-12-28 12:50:24', 0, 1);
 /*!40000 ALTER TABLE `session` ENABLE KEYS */;
 
 
@@ -127,7 +133,7 @@ CREATE TABLE IF NOT EXISTS `session_question` (
   CONSTRAINT `FK_session_question_session` FOREIGN KEY (`session_id`) REFERENCES `session` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
--- Dumping data for table evoter.session_question: ~4 rows (approximately)
+-- Dumping data for table evoter.session_question: ~5 rows (approximately)
 DELETE FROM `session_question`;
 /*!40000 ALTER TABLE `session_question` DISABLE KEYS */;
 INSERT INTO `session_question` (`question_id`, `session_id`) VALUES
@@ -223,19 +229,20 @@ CREATE TABLE IF NOT EXISTS `user` (
   PRIMARY KEY (`id`),
   KEY `user_FKIndex1` (`user_type_id`),
   CONSTRAINT `FK_user_user_type` FOREIGN KEY (`user_type_id`) REFERENCES `user_type` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=24 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=29 DEFAULT CHARSET=utf8;
 
 -- Dumping data for table evoter.user: ~6 rows (approximately)
 DELETE FROM `user`;
 /*!40000 ALTER TABLE `user` DISABLE KEYS */;
 INSERT INTO `user` (`id`, `user_type_id`, `email_address`, `username`, `passwd`) VALUES
-	(1, 2, 'paul.gibson@telecom-sudparis.eu', 'paul', '12345678'),
+	(1, 2, 'paul.gibson@telecom-sudparis.eu', 'paul_gibson', '12345678'),
 	(2, 2, 'jean.luc@telecom-sudparis.eu', 'jean', '12345678'),
 	(3, 3, 'nguyen.van@telecom-sudparis.eu', 'nvluong', '12345678'),
 	(4, 3, 'nguyen.mai@telecom-sudparis.eu', 'ntmai', '12345678'),
 	(21, 3, 'diem.bt@telecom-sudparis.eu', 'btdiem', '1234567890'),
 	(22, 2, 'tuan@yahoo.com', 'tuan', '123456789'),
-	(23, 1, 'someone@gmail.com', 'so', '98adsajdsa');
+	(23, 1, 'someone@gmail.com', 'so', '98adsajdsa'),
+	(28, 3, 'diemth@gmail.com', 'diembt12', 'tuilaai');
 /*!40000 ALTER TABLE `user` ENABLE KEYS */;
 
 

66ea3b2a8d9d9ed064edcd3a22bdb3e4d4c36083
btdiem <diembt@somwhere>
2014-02-01 03:54:59 +0100

    Move DAO objects called in Request to applicationContext

---
+++ eVoterServer/src/src/evoter/server/http/request/interfaces/ISessionRequest.java
@@ -0,0 +1,111 @@
+/**
+ * 
+ */
+package evoter.server.http.request.interfaces;
+
+import java.util.Map;
+import com.sun.net.httpserver.HttpExchange;
+
+import evoter.server.http.URIRequest;
+import evoter.share.dao.SessionDAO;
+import evoter.share.dao.UserDAO;
+import evoter.share.model.Question;
+import evoter.share.model.Session;
+import evoter.share.model.SessionUser;
+import evoter.share.model.Subject;
+
+/**
+ * @author btdiem
+ *
+ */
+public interface ISessionRequest {
+
+	public static final String BEAN_NAME = "sessionRequest";
+	/**
+	 * This method will select all {@link Session} of a specific {@link Subject} </br>
+	 * and the result will be added to response to client application </br>
+	 * There are two kind of requests. One is sent by teacher user and the other is sent by student user </br>
+	 * The first selection is made from session table. if a null or empty value is returned, that means </br>
+	 * the request is coming from student user application. So try to select all session in session_user table </br>
+	 * and get all session object from returned value above </br>
+	 * 
+	 * @param httpExchange {@link HttpExchange} communicates between server client application </br>
+	 * @param parameters contains : </br>
+	 * 	</li> SessionDAO.SUBJECT_ID
+	 * 	</li> {@link UserDAO#USER_KEY}
+	 */
+	public void doGetAll(HttpExchange httpExchange, Map<String,Object> parameters) ;
+
+	/**
+	 * Response client a {@link Session}  when receiving {@link URIRequest#VIEW_SESSION} request </br>
+	 * 
+	 * @param httpExchange {@link HttpExchange} communicates between server client application </br>
+	 * @param parameters request parameter map contains </br>
+	 * 	</li> SessionDAO.ID
+	 */
+	public void doView(HttpExchange httpExchange, Map<String,Object> parameters) ; 
+
+	/**
+	 * Change the status of {@link Session} to inactive </br>
+	 * when receiving {@link URIRequest#ACTIVE_SESSION}</br>
+	 * 
+	 * @param httpExchange {@link HttpExchange} communicates between server and client </br>
+	 * @param parameters contains : </br>
+	 *  </li> SessionDAO.ID
+	 *  </li> {@link UserDAO#USER_KEY}
+	 */
+	public void doActive(HttpExchange httpExchange, Map<String,Object> parameters) ;
+	
+	public void doAccept(HttpExchange httpExchange, Map<String,Object> parameters); 
+
+	/**
+	 * Update delete_indicator field of SESSION_USER table </br>
+	 * when receiving {@link URIRequest#DELETE_SESSION} request from client application</br>
+	 * to mark that this session is deleted by a user </br>
+	 * 
+	 * @param httpExchange {@link HttpExchange} communicates between server and client </br>
+	 * @param parameters contains : </br>
+	 * 	</li> SessionUserDAO.SESSION_ID
+	 *  </li> UserDAO.USER_KEY
+	 */
+	public void doDelete(HttpExchange httpExchange, Map<String,Object> parameters);
+	/**
+	 * Create a new {@link Question} object when receiving {@link URIRequest#CREATE_SESSION} </br>
+	 * The order of steps are: </br>
+	 * </li>Create a new {@link Session} and insert to SESSION table </br>
+	 * </li>Create a new {@link SessionUser}  and insert to SESSION_USER table </br>
+	 * @param httpExchange {@link HttpExchange} communicates between server and client </br>
+	 * @param parameters contains 
+	 * 		</li> {@link SessionDAO#CREATION_DATE}
+	 * 		</li> {@link SessionDAO#IS_ACTIVE}
+	 * 		</li> {@link SessionDAO#NAME}
+	 * 		</li> {@link SessionDAO#SUBJECT_ID}
+	 * 		</li> {@link UserDAO#USER_KEY}
+	 * 		</li> 
+	 */
+	public void doCreate(HttpExchange httpExchange, Map<String,Object> parameters); 
+
+	/**
+	 * Change the status of {@link Session} to inactive </br>
+	 * when receiving {@link URIRequest#INACTIVE_SESSION}</br>
+	 * 
+	 * @param httpExchange {@link HttpExchange} communicates between server and client </br>
+	 * @param parameters contains: </br>
+	 * 		</li> {@link SessionDAO#ID}
+	 * 		</li> {@link UserDAO#USER_KEY}
+	 */
+	public void doInActive(HttpExchange httpExchange, Map<String,Object> parameters) ;
+	
+	
+	/**
+	 * Update {@link SessionDAO#NAME} of {@link Session} </br> 
+	 * when receiving {@link URIRequest#UPDATE_SESSION} </br>
+	 * 
+	 * @param httpExchange {@link HttpExchange} communicates between server and client </br>
+	 * @param parameters contains </br>
+	 * 		</li> {@link SessionDAO#NAME}
+	 * 		</li> {@link SessionDAO#ID} 
+	 * 		</li> {@link UserDAO#USER_KEY}
+	 */
+	public  void doUpdate(HttpExchange httpExchange, Map<String,Object> parameters) ;
+}

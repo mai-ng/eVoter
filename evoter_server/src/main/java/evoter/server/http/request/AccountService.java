e38bddf21f7267d4e4692de46260faa4d0519676
btdiem <diembt@somwhere>
2014-02-11 13:58:16 +0100

    Refactor code for AccountService

--- evoter_server/src/main/java/evoter/server/http/request/AccountService.java
+++ evoter_server/src/main/java/evoter/server/http/request/AccountService.java
@@ -5,14 +5,13 @@ import java.util.List;
 import java.util.Map;
 
 import org.json.simple.JSONObject;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.test.context.transaction.TransactionConfiguration;
+import org.springframework.transaction.annotation.Transactional;
 //import org.springframework.test.annotation.Rollback;
 //import org.springframework.transaction.annotation.Transactional;
 
 import com.sun.net.httpserver.HttpExchange;
-
-import evoter.server.http.URIUtils;
 import evoter.server.http.request.interfaces.IAccountService;
 import evoter.share.dao.UserDAO;
 import evoter.share.model.User;
@@ -24,6 +23,8 @@ import evoter.share.utils.URIRequest;
  *
  */
 @Service
+@Transactional
+@TransactionConfiguration(defaultRollback=true)
 public class AccountService implements IAccountService{
 	
 	 List<String> userKeys = new ArrayList<String>();
@@ -31,10 +32,10 @@ public class AccountService implements IAccountService{
 	 String userKeyemp = "temp";
 	 private AccountService(){userKeys.add(userKeyemp);}
 	
-	 @Autowired
+	 //@Autowired
 	 private UserDAO userDAO;
 
-	 
+	
 	 public void setUserDAO(UserDAO userDAO){
 		 this.userDAO = userDAO;
 	 }
@@ -58,30 +59,39 @@ public class AccountService implements IAccountService{
 	
 	//set transaction and rollback here
 	@SuppressWarnings("unchecked")
-	public  void doLogin(HttpExchange exchange, Map<String,Object> parameters){
+	public  Object doLogin(Map<String,Object> parameters){
 		
-		String username = (String)parameters.get(UserDAO.USER_NAME);
-		String password = (String)parameters.get(UserDAO.PASSWORD);
 
 		try{
+			String username = (String)parameters.get(UserDAO.USER_NAME);
+			String password = (String)parameters.get(UserDAO.PASSWORD);
+
+			JSONObject response = new JSONObject();
 			
-			List<User> users = userDAO.findByProperty(new String[]{UserDAO.USER_NAME, UserDAO.PASSWORD}, 
+			List<User> users = userDAO.findByProperty(
+					new String[]{UserDAO.USER_NAME, UserDAO.PASSWORD}, 
 					new Object[]{username, password});
 			String userKey = null;
 			if (users != null && !users.isEmpty()){
-				userKey = users.get(0).generateUserKey(System.currentTimeMillis());
-				userKeys.add(userKey);
 				System.out.println("user exists in db");
+				userKey = users.get(0).generateUserKey(System.currentTimeMillis());
+				addUserKey(userKey);
+				response.put(UserDAO.USER_KEY, userKey);
+				return response;
 				
+			}else{
+				
+				return URIRequest.USER_NOT_EXIST_MESSAGE;
 			}
-			JSONObject object = new JSONObject();
-			object.put(UserDAO.USER_KEY, userKey);
-			URIUtils.writeResponse(object, exchange);
+			
+			//URIUtils.writeResponse(object, exchange);
 			
 		}catch(Exception e){
+			
 			e.printStackTrace();
 			
-			URIUtils.writeFailureResponse(exchange);
+			return URIRequest.FAILURE_MESSAGE;
+			//URIUtils.writeFailureResponse(exchange);
 		}
 
 	}
@@ -91,9 +101,9 @@ public class AccountService implements IAccountService{
 	 * @param parameters contain {@link UserDAO#USER_KEY} </br>
 	 * @return true if {@link UserDAO#USER_KEY} exists, otherwise return false </br>
 	 */
-	public  boolean hasUserKey(Map<String,Object> parameters){
+	public  boolean hasUserKey(String userKey){
 		
-		return userKeys.contains(parameters.get(UserDAO.USER_KEY));
+		return userKeys.contains(userKey);
 		//return true;
 	}
 
@@ -104,15 +114,20 @@ public class AccountService implements IAccountService{
 	 * @param exchange {@link HttpExchange} communicate between clients and server </br>
 	 * @param parameters contains {@link UserDAO#USER_KEY} </br>
 	 */
-	public  void doLogout(HttpExchange exchange, Map<String,Object> parameters) {
+	@Override
+	public  Object doLogout(Map<String,Object> parameters) {
 		
-		if (hasUserKey(parameters)){
+		if (hasUserKey((String)parameters.get(UserDAO.USER_KEY))){
 			
 			userKeys.remove(parameters.get(UserDAO.USER_KEY));
-			URIUtils.writeSuccessResponse(exchange);
+			
+			return URIRequest.SUCCESS_MESSAGE;
+			//URIUtils.writeSuccessResponse(exchange);
 			
 		}else{
-			URIUtils.writeFailureResponse(exchange);
+			
+			return URIRequest.FAILURE_MESSAGE;
+			//URIUtils.writeFailureResponse(exchange);
 		}
 	}
 
@@ -122,9 +137,8 @@ public class AccountService implements IAccountService{
 	 * @param parameters contains email address </br>
 	 */
 	
-	
-	public  void doResetPassword(HttpExchange httpExchange,
-			Map<String, Object> parameters) {
+	@Override
+	public  Object doResetPassword(Map<String, Object> parameters) {
 		
 		try{
 
@@ -133,15 +147,20 @@ public class AccountService implements IAccountService{
 			
 			List<User> userList = userDAO.findByEmail(email);
 			if (userList != null && !userList.isEmpty()){
-				URIUtils.writeResponse(URIRequest.EMAIL_EXIST_MESSAGE, httpExchange);
+				
+				return URIRequest.EMAIL_EXIST_MESSAGE;
+				//URIUtils.writeResponse(URIRequest.EMAIL_EXIST_MESSAGE, httpExchange);
 				
 			}else{
-				URIUtils.writeResponse(URIRequest.EMAIL_NOT_EXIST_MESSAGE, httpExchange);
+				
+				return URIRequest.EMAIL_NOT_EXIST_MESSAGE;
+				//URIUtils.writeResponse(URIRequest.EMAIL_NOT_EXIST_MESSAGE, httpExchange);
 			}
 			
 		}catch(Exception e){
 			
-			URIUtils.writeFailureResponse(httpExchange);
+			//URIUtils.writeFailureResponse(httpExchange);
+			return URIRequest.FAILURE_MESSAGE;
 		}
 		
 	}
@@ -160,47 +179,71 @@ public class AccountService implements IAccountService{
 	 * 
 	 */
 	
-	
-	public  void doRegister(HttpExchange httpExchange,
-			Map<String, Object> parameters) {
+	@Override
+	public  Object doRegister(Map<String, Object> parameters) {
 		
 		try{
 			
 			String username = (String)parameters.get(UserDAO.USER_NAME);
 			String email = (String)parameters.get(UserDAO.EMAIL);
-			long userTypeId = Long.valueOf((String)parameters.get(UserDAO.USER_TYPE_ID));
+			long userTypeId =(Long)parameters.get(UserDAO.USER_TYPE_ID);
 			
 			//search database if this username exist
 			List<User> checkUsername = userDAO.findByProperty(new String[]{UserDAO.USER_NAME, UserDAO.USER_TYPE_ID}
 															, new Object[]{username, userTypeId});
-			if (checkUsername != null && !checkUsername.isEmpty()){
-				URIUtils.writeResponse(URIRequest.USER_EXIST_MESSAGE, httpExchange);
-				return;
-			}
+			if (checkUsername != null && !checkUsername.isEmpty())
+				return URIRequest.USER_EXIST_MESSAGE;
+//				URIUtils.writeResponse(URIRequest.USER_EXIST_MESSAGE, httpExchange);
+//				return;
+			
 			
 			//search database if this email exists
 			List<User> checkEmail = userDAO.findByProperty(new String[]{UserDAO.EMAIL, UserDAO.USER_TYPE_ID}
 														, new Object[]{email, userTypeId});
-			if (checkEmail != null && !checkEmail.isEmpty()){
-				URIUtils.writeResponse(URIRequest.EMAIL_EXIST_MESSAGE, httpExchange);
-				return;
-			}
+			if (checkEmail != null && !checkEmail.isEmpty())
+				return URIRequest.EMAIL_EXIST_MESSAGE;
+//			{
+//				URIUtils.writeResponse(URIRequest.EMAIL_EXIST_MESSAGE, httpExchange);
+//				return;
+//			}
 			
 			//create User object and insert to database
 			String password = (String)parameters.get(UserDAO.PASSWORD);
 			User user = new User(username, password, email, userTypeId);
 			userDAO.insert(user);
 
-			URIUtils.writeSuccessResponse(httpExchange);
+			return URIRequest.SUCCESS_MESSAGE;
+			//URIUtils.writeSuccessResponse(httpExchange);
 				
 		}catch(Exception e){
 
-			System.err.println(e);
-			URIUtils.writeFailureResponse(httpExchange);
+			e.printStackTrace();
+			//System.err.println(e);
+			//URIUtils.writeFailureResponse(httpExchange);
+			return URIRequest.FAILURE_MESSAGE;
 			
 		}
 		
 	}
+	/*
+	 * (non-Javadoc)
+	 * @see evoter.server.http.request.interfaces.IAccountService#addUserKey(java.lang.String)
+	 */
+	@Override
+	public void addUserKey(String userKey) {
+		userKeys.add(userKey);
+	}
+	/*
+	 * (non-Javadoc)
+	 * @see evoter.server.http.request.interfaces.IAccountService#removeUserKey(java.lang.String)
+	 */
+	@Override
+	public void removeUserKey(String userKey) {
+		userKeys.remove(userKey);
+	}
+	
+	
+	
 	
 
 }

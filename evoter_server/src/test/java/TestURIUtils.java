11e95b2fed19091b17974f1dc3b5ad1033fa57e5
btdiem <diembt@somwhere>
2014-02-13 00:38:54 +0100

    Create unit test cases for URIUtils

---
+++ evoter_server/src/test/java/TestURIUtils.java
@@ -0,0 +1,22 @@
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import evoter.server.http.URIUtils;
+
+
+public class TestURIUtils {
+
+	URIUtils utils;
+	
+	@Test
+	public void test_fixedEncodeValueParameter() {
+		
+		String before = "2013-56-12+02%3A56%3A12";
+		String after = URIUtils.fixedEncodeValueParameter(before);
+		assertEquals(after, "2013-56-12 02:56:12");
+	}
+
+}

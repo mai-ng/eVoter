8cc23c3737375e0eb4bea3ac7c21ad94cb6830c7
btdiem <diembt@somwhere>
2014-02-13 00:40:35 +0100

    Add requests to user service

--- evoter_server/src/main/java/evoter/server/http/URIUtils.java
+++ evoter_server/src/main/java/evoter/server/http/URIUtils.java
@@ -39,185 +39,250 @@ public class URIUtils {
 	public static boolean isLoginRequest(String URIString){
 		
 		return (URIString != null) 
-			&& URIString.contains(((HttpConnection)BeanDAOFactory.getBean("httpConnection")).getContext()) 
+			&& URIString.contains(((HttpConnection)BeanDAOFactory.
+					getBean("httpConnection")).getContext()) 
 			&& URIString.endsWith(URIRequest.LOGIN); 
 	}
 
 	public static boolean isLogoutRequest(String URIString){
 		
 		return (URIString != null) 
-				&& URIString.contains(((HttpConnection)BeanDAOFactory.getBean("httpConnection")).getContext()) 
+				&& URIString.contains(((HttpConnection)BeanDAOFactory.
+						getBean("httpConnection")).getContext()) 
 				&& URIString.endsWith(URIRequest.LOGOUT); 
 	}
 	
 	public static boolean isGetAllSubjectRequest(String URIString){
 		
 		return (URIString != null) 
-				&& URIString.contains(((HttpConnection)BeanDAOFactory.getBean("httpConnection")).getContext()) 
+				&& URIString.contains(((HttpConnection)BeanDAOFactory.
+						getBean("httpConnection")).getContext()) 
 				&& URIString.endsWith(URIRequest.GET_ALL_SUBJECT); 
 	}
 	
 	public static boolean isViewSubjectRequest(String URIString){
 		
 		return (URIString != null) 
-				&& URIString.contains(((HttpConnection)BeanDAOFactory.getBean("httpConnection")).getContext()) 
+				&& URIString.contains(((HttpConnection)BeanDAOFactory.
+						getBean("httpConnection")).getContext()) 
 				&& URIString.endsWith(URIRequest.VIEW_SUBJECT); 
 	}
 	
 	public static boolean isDeleteSubjectRequest(String URIString){
 		
 		return (URIString != null) 
-				&& URIString.contains(((HttpConnection)BeanDAOFactory.getBean("httpConnection")).getContext()) 
+				&& URIString.contains(((HttpConnection)BeanDAOFactory.
+						getBean("httpConnection")).getContext()) 
 				&& URIString.endsWith(URIRequest.DELETE_SUBJECT); 
 	}
 	
 	public static boolean isSearchSubjectRequest(String URIString){
 		
 		return (URIString != null) 
-				&& URIString.contains(((HttpConnection)BeanDAOFactory.getBean("httpConnection")).getContext()) 
+				&& URIString.contains(((HttpConnection)BeanDAOFactory.
+						getBean("httpConnection")).getContext()) 
 				&& URIString.endsWith(URIRequest.SEARCH_SUBJECT); 
 	}
 	
 	public static boolean isGetAllSessionRequest(String URIString){
 		
 		return (URIString != null) 
-				&& URIString.contains(((HttpConnection)BeanDAOFactory.getBean("httpConnection")).getContext()) 
+				&& URIString.contains(((HttpConnection)BeanDAOFactory.
+						getBean("httpConnection")).getContext()) 
 				&& URIString.endsWith(URIRequest.GET_ALL_SESSION); 
 	}
 	
 	public static boolean isViewSessionRequest(String URIString){
 		
 		return (URIString != null) 
-				&& URIString.contains(((HttpConnection)BeanDAOFactory.getBean("httpConnection")).getContext()) 
+				&& URIString.contains(((HttpConnection)BeanDAOFactory.
+						getBean("httpConnection")).getContext()) 
 				&& URIString.endsWith(URIRequest.VIEW_SESSION); 
 	}
 	
 	public static boolean isCreateSessionRequest(String URIString){
 		
 		return (URIString != null) 
-				&& URIString.contains(((HttpConnection)BeanDAOFactory.getBean("httpConnection")).getContext()) 
+				&& URIString.contains(((HttpConnection)BeanDAOFactory.
+						getBean("httpConnection")).getContext()) 
 				&& URIString.endsWith(URIRequest.CREATE_SESSION); 
 	}
 	
 	public static boolean isActiveSessionRequest(String URIString){
 		
 		return (URIString != null) 
-				&& URIString.contains(((HttpConnection)BeanDAOFactory.getBean("httpConnection")).getContext()) 
+				&& URIString.contains(((HttpConnection)BeanDAOFactory.
+						getBean("httpConnection")).getContext()) 
 				&& URIString.endsWith(URIRequest.ACTIVE_SESSION); 
 	}
 
 	public static boolean isInActiveSessionRequest(String URIString){
 		
 		return (URIString != null) 
-				&& URIString.contains(((HttpConnection)BeanDAOFactory.getBean("httpConnection")).getContext()) 
+				&& URIString.contains(((HttpConnection)BeanDAOFactory.
+						getBean("httpConnection")).getContext()) 
 				&& URIString.endsWith(URIRequest.INACTIVE_SESSION); 
 	}
 
 	public static boolean isDeleteSessionRequest(String URIString){
 		
 		return (URIString != null) 
-				&& URIString.contains(((HttpConnection)BeanDAOFactory.getBean("httpConnection")).getContext()) 
+				&& URIString.contains(((HttpConnection)BeanDAOFactory.
+						getBean("httpConnection")).getContext()) 
 				&& URIString.endsWith(URIRequest.DELETE_SESSION); 
 	}
 	
 	public static boolean isUpdateSessionRequest(String URIString){
 		
 		return (URIString != null) 
-				&& URIString.contains(((HttpConnection)BeanDAOFactory.getBean("httpConnection")).getContext()) 
+				&& URIString.contains(((HttpConnection)BeanDAOFactory.
+						getBean("httpConnection")).getContext()) 
 				&& URIString.endsWith(URIRequest.UPDATE_SESSION); 
 	}
 	
 	public static boolean isAcceptSessionRequest(String URIString){
 		
 		return (URIString != null) 
-				&& URIString.contains(((HttpConnection)BeanDAOFactory.getBean("httpConnection")).getContext()) 
+				&& URIString.contains(((HttpConnection)BeanDAOFactory.
+						getBean("httpConnection")).getContext()) 
 				&& URIString.endsWith(URIRequest.ACCEPT_SESSION); 
 	}
 
 	public static boolean isSearchSessionRequest(String URIString){
 		
 		return (URIString != null) 
-				&& URIString.contains(((HttpConnection)BeanDAOFactory.getBean("httpConnection")).getContext()) 
+				&& URIString.contains(((HttpConnection)BeanDAOFactory.
+						getBean("httpConnection")).getContext()) 
 				&& URIString.endsWith(URIRequest.SEARCH_SESSION); 
 	}
 
 	public static boolean isGetAllQuestionRequest(String URIString){
 		
 		return (URIString != null) 
-			&& URIString.contains(((HttpConnection)BeanDAOFactory.getBean("httpConnection")).getContext()) 
+			&& URIString.contains(((HttpConnection)BeanDAOFactory.
+					getBean("httpConnection")).getContext()) 
 			&& URIString.endsWith(URIRequest.GET_ALL_QUESTION); 
 	}
 	
 	public static boolean isViewQuestionRequest(String URIString){
 		
 		return (URIString != null) 
-			&& URIString.contains(((HttpConnection)BeanDAOFactory.getBean("httpConnection")).getContext()) 
+			&& URIString.contains(((HttpConnection)BeanDAOFactory.
+					getBean("httpConnection")).getContext()) 
 			&& URIString.endsWith(URIRequest.VIEW_QUESTION); 
 	}
 	
 	public static boolean isCreateQuestionRequest(String URIString){
 		
 		return (URIString != null) 
-			&& URIString.contains(((HttpConnection)BeanDAOFactory.getBean("httpConnection")).getContext()) 
+			&& URIString.contains(((HttpConnection)BeanDAOFactory.
+					getBean("httpConnection")).getContext()) 
 			&& URIString.endsWith(URIRequest.CREATE_QUESTION); 
 	}
 	
 	public static boolean isDeleteQuestionRequest(String URIString){
 		
 		return (URIString != null) 
-			&& URIString.contains(((HttpConnection)BeanDAOFactory.getBean("httpConnection")).getContext()) 
+			&& URIString.contains(((HttpConnection)BeanDAOFactory.
+					getBean("httpConnection")).getContext()) 
 			&& URIString.endsWith(URIRequest.DELETE_QUESTION); 
 	}
 
 	public static boolean isUpdateQuestionRequest(String URIString){
 		
 		return (URIString != null) 
-			&& URIString.contains(((HttpConnection)BeanDAOFactory.getBean("httpConnection")).getContext()) 
+			&& URIString.contains(((HttpConnection)BeanDAOFactory.
+					getBean("httpConnection")).getContext()) 
 			&& URIString.endsWith(URIRequest.UPDATE_QUESTION); 
 	}
 	
 	public static boolean isSendQuestionRequest(String URIString){
 		
 		return (URIString != null) 
-			&& URIString.contains(((HttpConnection)BeanDAOFactory.getBean("httpConnection")).getContext()) 
+			&& URIString.contains(((HttpConnection)BeanDAOFactory.
+					getBean("httpConnection")).getContext()) 
 			&& URIString.endsWith(URIRequest.SEND_QUESTION); 
 	}
 	
 	public static boolean isGetQuestionRequest(String URIString){
 		
 		return (URIString != null) 
-				&& URIString.contains(((HttpConnection)BeanDAOFactory.getBean("httpConnection")).getContext()) 
+				&& URIString.contains(((HttpConnection)BeanDAOFactory.
+						getBean("httpConnection")).getContext()) 
 				&& URIString.endsWith(URIRequest.GET_QUESTION); 
 	}
 
 	public static boolean isStopSendQuestionRequest(String URIString){
 		
 		return (URIString != null) 
-				&& URIString.contains(((HttpConnection)BeanDAOFactory.getBean("httpConnection")).getContext()) 
+				&& URIString.contains(((HttpConnection)BeanDAOFactory.
+						getBean("httpConnection")).getContext()) 
 				&& URIString.endsWith(URIRequest.STOP_SEND_QUESTION); 
 	}
 	
 	public static boolean isSearchQuestionRequest(String URIString){
 		
 		return (URIString != null) 
-				&& URIString.contains(((HttpConnection)BeanDAOFactory.getBean("httpConnection")).getContext()) 
+				&& URIString.contains(((HttpConnection)BeanDAOFactory.
+						getBean("httpConnection")).getContext()) 
 				&& URIString.endsWith(URIRequest.SEARCH_QUESTION); 
 	}
 	
 	public static boolean isGetLatestQuestionRequest(String URIString){
 		
 		return (URIString != null) 
-			&& URIString.contains(((HttpConnection)BeanDAOFactory.getBean("httpConnection")).getContext()) 
+			&& URIString.contains(((HttpConnection)BeanDAOFactory.
+					getBean("httpConnection")).getContext()) 
 			&& URIString.endsWith(URIRequest.GET_LATEST_QUESTION); 
 	}
 
 	public static boolean isGetStatisticsRequest(String URIString){
 		
 		return (URIString != null) 
-			&& URIString.contains(((HttpConnection)BeanDAOFactory.getBean("httpConnection")).getContext()) 
+			&& URIString.contains(((HttpConnection)BeanDAOFactory.
+					getBean("httpConnection")).getContext()) 
 			&& URIString.endsWith(URIRequest.GET_STATISTICS); 
 	}
-	
+	/**
+	 * 
+	 * @param URIString
+	 * @return
+	 */
+	public static boolean isGetAllUserRequest(String URIString){
+		
+		return (URIString != null) 
+			&& URIString.contains(((HttpConnection)BeanDAOFactory.
+					getBean("httpConnection")).getContext()) 
+			&& URIString.endsWith(URIRequest.GET_ALL_USER); 
+	}
+	public static boolean isCreateUserRequest(String URIString){
+		
+		return (URIString != null) 
+			&& URIString.contains(((HttpConnection)BeanDAOFactory.
+					getBean("httpConnection")).getContext()) 
+			&& URIString.endsWith(URIRequest.CREATE_USER); 
+	}	
+	public static boolean isEditUserRequest(String URIString){
+		
+		return (URIString != null) 
+			&& URIString.contains(((HttpConnection)BeanDAOFactory.
+					getBean("httpConnection")).getContext()) 
+			&& URIString.endsWith(URIRequest.EDIT_USER); 
+	}	
+	public static boolean isDeleteUserRequest(String URIString){
+		
+		return (URIString != null) 
+			&& URIString.contains(((HttpConnection)BeanDAOFactory.
+					getBean("httpConnection")).getContext()) 
+			&& URIString.endsWith(URIRequest.DELETE_USER); 
+	}	
+	public static boolean isChangeApproveUserRequest(String URIString){
+		
+		return (URIString != null) 
+			&& URIString.contains(((HttpConnection)BeanDAOFactory.
+					getBean("httpConnection")).getContext()) 
+			&& URIString.endsWith(URIRequest.CHANGE_APPROVE_USER); 
+	}	
 	public static Map<String, Object> getParameters(HttpExchange exchange) {
 		
 		Map<String,Object> parameters = new HashMap<String, Object>();

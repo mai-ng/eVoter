d5325c9da665508a7c196ea06e6d42e42e3a8061
btdiem <diembt@somwhere>
2014-02-19 16:40:04 +0100

    try to log output instead of using system.out

--- evoter_server/src/main/java/evoter/server/http/Server.java
+++ evoter_server/src/main/java/evoter/server/http/Server.java
@@ -10,16 +10,18 @@ import com.sun.net.httpserver.HttpServer;
 
 import evoter.server.dao.impl.BeanDAOFactory;
 import evoter.server.http.HttpConnection;
+import org.apache.log4j.Logger;
 
 /**
  * This is a main class to start evoter server </br>
  * The connection information is provided by {@link HttpConnection} bean loaded by {@link BeanDAOFactory} </br>
- * Create a {@link HttpServer} and handle mult-thread {@link HttpRequest} </br>
+ * Create a {@link HttpServer} and handle multi-thread {@link HttpRequest} </br>
  * @author btdiem </br>
  *
  */
 public class Server {
 
+	static Logger log = Logger.getLogger(Server.class);
 	/**
 	 * @param args
 	 */
@@ -31,12 +33,16 @@ public class Server {
 			HttpContext  context = server.createContext("/"+http.getContext(), new ServerHandler());
 			server.setExecutor(Executors.newCachedThreadPool()); // creates a default executor
 			server.start();
-			System.out.println("Listening for connections...");
-			System.out.println(context.getPath());
-			System.out.println(server.getAddress());
+			
+			System.out.println("Listening for connections..." + server.getAddress() + context.getPath());
+			
+			log.debug("Listening for connections...");
+			log.debug(context.getPath());
+			log.debug(server.getAddress());
+			
 			
 		} catch (Exception e) {
-			System.err.println("exception: " + e);
+			log.error(e);
 		}
 
 

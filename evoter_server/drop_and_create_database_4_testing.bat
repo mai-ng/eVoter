25d74a2907be1fd94aadd9b79ff5d2c4070dfb6b
btdiem <diembt@somwhere>
2014-02-19 16:37:47 +0100

    Add drop_and_create_database_4_running.bat and drop_and_create_database_4_testing.bat

---
+++ evoter_server/drop_and_create_database_4_testing.bat
@@ -0,0 +1,25 @@
+@echo off
+
+
+rrem Enter the director installing mysql server
+rem for example : C:\Program Files\MySQL\MySQL Server 5.6
+echo Enter the installed directorty of MySQL
+echo For example : C:\Program Files\MySQL\MySQL Server 5.6
+set /p MYSQL_HOME= MySQL Home=?
+
+echo.
+rem Enter host name where database will be installed and run
+echo Enter the database host name
+echo For example : localhost
+set /p HOST_NAME= host name=?
+
+echo.
+rem Enter database user name that accesses database 
+echo Enter user name to access the database
+set /p USER= username=?
+
+echo.
+echo Creating database to run unit test cases.....
+"%MYSQL_HOME%\bin\mysql" -h %HOST_NAME% -u %USER% -p < %~dp0db\create_tables_test.sql
+
+pause
